da02f031208bd99fb3ba957eae1ebf6c78480555
adajitdubey4211 <adajitdubey4211@gmail.com>
2024-11-14 17:43:01 +0530

    Add files via upload

---
+++ email_slicer.py
@@ -0,0 +1,10 @@
+print("Please enter your Email Id: ")
+email = input().strip()
+
+if email.find("@") != -1:
+    username = email[:email.index("@")]     # username value = from 0 to index < @
+    domain = email[email.index("@") + 1:]   # domain value = from index > @ to end
+    print("Your username is: ", username)
+    print("Your domain is: ", domain)
+else:
+    print("Please enter a valid Email Id.")
